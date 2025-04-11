efd1b4ee95ba73609d3e9cf6f9ffce0253ab46fe
Cristian Isaac Vargas Garcia <civargas34@gmail.com>
2025-04-11 12:05:01 -0400

    cambio en la implementacion de tailwind a la verison 4.0

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -1,14 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
- 
-  return (
-    <>
-       <div className="p-4 bg-blue-500 text-white rounded-xl">
-          ¡Hola!
-       </div>
-    </>
-  )
+	return (
+		<>
+			<div className='p-4 bg-blue-500 text-white rounded-xl'>¡Hola!</div>
+		</>
+	);
 }
 
-export default App
+export default App;
