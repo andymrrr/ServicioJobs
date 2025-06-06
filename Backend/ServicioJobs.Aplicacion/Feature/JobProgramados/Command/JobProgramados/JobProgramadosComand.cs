fa79a25d7e6f993ca9e8d976504e960b89744a50
Andy Reyes <42513348+andymrrr@users.noreply.github.com>
2025-06-06 16:59:38 -0400

    Busqueda

---
+++ Backend/ServicioJobs.Aplicacion/Feature/JobProgramados/Command/JobProgramados/JobProgramadosComand.cs
@@ -0,0 +1,21 @@
+﻿using ServicioJobs.Aplicacion.Feature.Parametros.Dto;
+using ServicioJobs.Modelos.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace ServicioJobs.Aplicacion.Feature.Programados.Command.JobProgramados
+{
+    internal class JobProgramadosComand
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        public string Url { get; set; }
+        public List<ParametroDto> JobParametro { get; set; }
+        public string Crontab { get; set; }
+        public string CorreoNotificar { get; set; }
+        public int? ReintentosPermitidos { get; set; }
+        public int? PeriodoReintento { get; set; }
+
+
+    }
+}
